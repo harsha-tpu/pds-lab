537f299bf727bf8b2243b29b508f1a0580550a35
Harshavardhan S U <126003105@sastra.ac.in>
2025-09-01 14:13:37 +0530

    Refactor MPI program to display rank and size
    Updated main function to include rank and size variables, and modified print statements to show number of processors.

--- hello.c
+++ hello.c
@@ -1,10 +1,26 @@
-#include <stdio> 
+#include <stdio.h> 
 #include <mpi.h> 
 
-void main(int argc, char* argv[]) {
-  int myRank; 
+void main(int argc, char** argv) {
+  int rank, size; 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-  printf("Hello from node %d\n", myRank);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  printf("Hello from node %d\n", rank);
+  printf("No of processors: %d\n", size);
   MPI_Finalize();
 }
+
+
+//---------------------OUTPUT-----------------------
+
+[cse7e23@sastra-masternode ~]$ mpicc hello.c -o out
+[cse7e23@sastra-masternode ~]$ mpirun -np 4 out
+Hello from node 0
+No of processors: 4
+Hello from node 1
+No of processors: 4
+Hello from node 3
+No of processors: 4
+Hello from node 2
+No of processors: 4
