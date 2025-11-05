15a6712c162428f1c3eda2213f67f6b600dd07a8
Harshavardhan S U <126003105@sastra.ac.in>
2025-11-05 23:09:19 +0530

    Refactor Byzantine fault detection in MPI program
    Refactor Byzantine fault detection logic and improve MPI communication.

--- byzantine.c
+++ byzantine.c
@@ -3,47 +3,74 @@
  * Nodes send their "vote" (rank) to each other. Faulty nodes send random values.
  * Each node collects votes from others and applies majority rule to detect faulty nodes.
  */
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <stdbool.h>
-#include <mpi.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
 int main(int argc, char** argv) {
-  MPI_Init(&argc, &argv);
-  int rank, size;
-  MPI_Comm_rank(MPI_COMM_WORLD, rank);
-  MPI_Comm_size(MPI_COMM_WORLD, size);
-  bool isFaulty = false;
-  if (rank == 2 || rank == 5) {
-    isFaulty = true;
-    printf("P%d: I am a faulty node!\n", rank);
-  }
-  // each process hold votes of every process
-  int votes[size];
+    MPI_Init(&argc, &argv);
 
-  // sending their votes
-  if (!isFaulty) { // good nodes
-    for (int i = 0; i < size; i++) {
-      if (i! = rank) MPI_Send(&rank, 1, MPI_INT, i, 101, MPI_COMM_WORLD);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    int faulty = 0;
+    if (rank == 2) faulty = 1;
+    if (faulty) {
+        printf("P%d is faulty, find me!\n", rank);
+        fflush(stdout);
     }
-    // receive votes from all other nodes
+
+    MPI_Status status;
+
+    // --- Stage 1: Each process sends one integer to every other ---
+    int recv[size];
     for (int i = 0; i < size; i++) {
-      if (i == rank) {
-        votes[i] = rank;
-      } else {
-        MPI_Status status; 
-        MPI_Recv(&votes[i], 1, MPI_INT, i, 101, MPI_COMM_WORLD, &status);
-      }
+        if (i == rank) {
+            recv[i] = rank;
+            continue;
+        }
+
+        int send_val = faulty ? (rand() % 100) : rank;
+        MPI_Sendrecv(&send_val, 1, MPI_INT, i, 0,
+                     &recv[i], 1, MPI_INT, i, 0,
+                     MPI_COMM_WORLD, &status);
     }
-  } else {
-    int randomValue;
+
+    // --- Stage 2: Exchange entire recv[] arrays pairwise ---
+    int matrix[size][size];
     for (int i = 0; i < size; i++) {
-      randomValue = (rand() + rank * rank) % 100; 
-      MPI_Send(&randomValue, 1, MPI_INT, i, 101, MPI_COMM_WORLD);
+        if (i == rank) {
+            for (int j = 0; j < size; j++)
+                matrix[i][j] = recv[j]; // self-copy
+            continue;
+        }
+
+        MPI_Sendrecv(recv, size, MPI_INT, i, 1,
+                     matrix[i], size, MPI_INT, i, 1,
+                     MPI_COMM_WORLD, &status);
     }
-  }
-  
-  MPI_Finalize();
-  return 0; 
+
+    // --- Stage 3: Fault detection ---
+    for (int i = 0; i < size; i++) {
+        int count = 0;
+        for (int j = 0; j < size - 1; j++) {
+            int votes = 0;
+            for (int k = 0; k < size - 1; k++) {
+                if (matrix[j][i] == matrix[k][i])
+                    votes++;
+            }
+            if (votes > (size / 2)) {
+                count = 1;
+                break;
+            }
+        }
+        if (!count) {
+            printf("P%d suspects %d is faulty!\n", rank, i);
+            fflush(stdout);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
 }
-
